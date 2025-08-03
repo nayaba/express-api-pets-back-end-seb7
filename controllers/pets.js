aaca7b4c81611739519dbd12738d27f81f2f3d0d
Nabila Ayaba <nabilaayaba@gmail.com>
2025-08-03 11:14:37 +0300

    Add index route

--- controllers/pets.js
+++ controllers/pets.js
@@ -4,8 +4,24 @@ const router = express.Router()
 
 
 // GET ROUTE (TEST)
-router.get('/', (req, res) => {
-    res.send('🙃')
+router.get('/', async (req, res) => {
+    try {
+        const foundPets = await Pet.find()
+        res.json(foundPets)
+    } catch(err) {
+        res.json({ msg: err.message })
+    }
+})
+
+// POST A NEW PET
+router.post('/', async (req, res) => {
+    try {
+        const createdPet = await Pet.create(req.body)
+        res.json(createdPet)
+    } catch (err) {
+        const message = { msg: err.message }
+        res.json(message)
+    }
 })
 
 
